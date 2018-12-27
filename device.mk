3e4da7512775852d4a378204a55201a3c1150265
Henrique Pereira <pereirah2001@gmail.com>
2018-12-27 13:13:49 +0000

    gemini: Remove custom thermal for Mi 5 Lite
    * We're using the same clock freqs on all Mi 5's now, and the different
      thermal for Mi 5 Lite is causing problems with charging. Also with the
      same freqs there's no reason to keep it.
    
    * Let's use just the thermal-engine-8996.conf from now on.
    
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- device.mk
+++ device.mk
@@ -363,7 +363,6 @@ PRODUCT_PACKAGES += \
     thermal.msm8996
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8996-lite.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-lite.conf \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
 
 # TextClassifier
