f20bfc65e9b73190efec970447692a59bf6b1c95
RadixCube <radixcube@gmail.com>
2019-01-01 19:38:31 +0000

    Revert "Update hidl.mk"
    This reverts commit ae7dd6ed6f645b9bfb295875b23f019b8c7dc899.

--- hidl.mk
+++ hidl.mk
@@ -24,9 +24,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    android.hardware.camera.provider@2.4-service
 
 # Display HIDL
 PRODUCT_PACKAGES += \
@@ -38,9 +36,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.display.color@1.0-impl \
-    vendor.display.color@1.0-service \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    vendor.display.color@1.0-service
 
 # DRM HIDL
 PRODUCT_PACKAGES += \
