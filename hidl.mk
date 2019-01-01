ae7dd6ed6f645b9bfb295875b23f019b8c7dc899
Stephen Strange <radixcube@gmail.com>
2019-01-01 10:24:28 +0530

    Update hidl.mk

--- hidl.mk
+++ hidl.mk
@@ -24,7 +24,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Display HIDL
 PRODUCT_PACKAGES += \
@@ -36,7 +38,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.display.color@1.0-impl \
-    vendor.display.color@1.0-service
+    vendor.display.color@1.0-service \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.0_vendor
 
 # DRM HIDL
 PRODUCT_PACKAGES += \
