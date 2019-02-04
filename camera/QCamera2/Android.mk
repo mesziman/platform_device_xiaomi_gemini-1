ec3c71d32b68c5d72b42e2a97432ea871c7076c0
RadixCube <radixcube@gmail.com>
2019-02-04 11:01:26 +0000

    Revert "msm8996-common: camera: Fixes to OSS camera hal to build in LineageOS tree"
    This reverts commit d0c28b38ab3914bac765cc6f6c99d4f47fddb474.

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -89,15 +89,15 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
-        $(call project-path-for,qcom-media)/mm-core/inc \
+        hardware/qcom/media/libstagefrighthw \
+        hardware/qcom/media/mm-core/inc \
         system/core/include/cutils \
         system/core/include/system \
         system/media/camera/include/system
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        $(LOCAL_PATH)/HAL
+        hardware/qcom/camera/QCamera2/HAL
 
 LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
@@ -117,8 +117,8 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libqservice
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync
+        hardware/qcom/display/libqservice
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 ifeq ($(USE_DISPLAY_SERVICE),true)
